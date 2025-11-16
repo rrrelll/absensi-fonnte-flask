73e542692cb29c6058843eae8868310b34a72916
farel <akbarfarel802@gmail.com>
2025-11-17 02:48:52 +0700

    ganti port agar sesuai 5000

--- app.py
+++ app.py
@@ -79,4 +79,6 @@ def home():
 # Jalankan untuk Lokal
 # =========================================================
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
